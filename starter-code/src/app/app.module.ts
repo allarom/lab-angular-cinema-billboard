b2ec4b615be4f1db1ade1b07deddfb8aac0357c8
Alla Roman <alla.roman.mail@gmail.com>
2018-02-21 21:49:54 +0100

    Iteration 2

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -4,17 +4,33 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyHomeComponentComponent } from './components/my-home-component/my-home-component.component';
+import { MyMovieComponentComponent } from './components/my-movie-component/my-movie-component.component';
+
+import { Routes, RouterModule } from "@angular/router";
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '/home',  component: MyHomeComponentComponent },
+  { path: '/movie/:id', component: MyMovieComponentComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponentComponent,
+    MyMovieComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
--- starter-code/src/app/services/movies.service.ts
+++ starter-code/src/app/services/movies.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MoviesService {
+  movies: Array<Object>;
+  movie: Object;
 
   constructor() { }
 
+  getMovies() {
+    return this.movies;
+  }
+
+  getMovie(id) {
+    return this.movie;
+  }
 }
